69d48370e6fce5438791d716959974a1495322e7
gustavo <gustavolourenco6522@gmail.com>
2026-03-10 08:51:02 -0300

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+ #calculadora
+number_1 = int(input('primeiro numero: '))
+number_2 = int(input('segundo numero: '))
+operacao = input("operacao: ")
+
+if operacao == '+':
+    print(number_1 + number_2)
+
+if operacao == '-':
+    print(number_1 - number_2) 
+
+if operacao == '/':
+    print(number_1 / number_2)
+
+if operacao == '*':
+    print(number_1 * number_2) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
